b2f9cc5ada85905cfc80923096b630569ee04f27
tahfeezmizan <mizanahmed5316@gmail.com>
2024-08-16 00:58:52 +0600

    add search by product name

--- server-site/index.js
+++ server-site/index.js
@@ -31,13 +31,24 @@ async function run() {
             try {
                 const page = parseInt(req.query.page) || 0;
                 const size = parseInt(req.query.size) || 10;
+                const search = req.query.search || ''; // default to empty string if no search query
 
-                const result = await productCollection.find()
+                // Create a query object
+                const query = search
+                    ? { productName: { $regex: search, $options: 'i' } } // 'i' for case-insensitive search
+                    : {};
+
+                const result = await productCollection.find(query)
                     .skip(page * size)
                     .limit(size)
                     .toArray();
 
-                res.send(result);
+                const totalProducts = await productCollection.countDocuments(query); // Count the total products based on the search query
+
+                res.json({
+                    products: result,
+                    count: totalProducts, // Send the count of products back to the frontend
+                });
             } catch (error) {
                 console.error('Error fetching products data:', error);
                 res.status(500).json({ message: 'Internal server error', error: error.message });
@@ -45,9 +56,11 @@ async function run() {
         });
 
 
+
         // pasinition 
         app.get('/productcount', async (req, res) => {
             const count = await productCollection.estimatedDocumentCount();
+            // console.log(count);
             res.send({ count })
         })
 
